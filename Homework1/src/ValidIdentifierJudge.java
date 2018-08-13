b0ee7ad3a46132054ed48c19bbebdaeb6395e34c
Harry Chen <i@harrychen.xyz>
2018-08-13 20:23:58 +0800

    Complete Homework 1
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- Homework1/src/ValidIdentifierJudge.java
+++ Homework1/src/ValidIdentifierJudge.java
@@ -1,17 +1,23 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class ValidIdentifierJudge {
 
-    private static boolean validIdentifier(String s){
-        if (Character.isJavaIdentifierStart(s.charAt(0))){
-            return s.chars().skip(1).allMatch(Character::isJavaIdentifierPart);
-        } else {
+    private static final String[] RESERVED = {"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float", "for", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while", "false", "null", "true", "goto"};
+    private static final List<String> RESERVED_LIST = List.of(RESERVED);
+
+    private static boolean validIdentifier(String s) {
+        if (RESERVED_LIST.contains(s)) {
             return false;
         }
+        if (!Character.isJavaIdentifierStart(s.charAt(0))) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isJavaIdentifierPart);
     }
 
     public static void main(String[] args) {
         var in = new Scanner(System.in);
-        System.out.println(validIdentifier(in.next()) ? 0 : -1);
+        System.out.println(validIdentifier(in.nextLine().trim()) ? 0 : -1);
     }
 }
